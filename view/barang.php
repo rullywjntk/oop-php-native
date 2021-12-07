a3860a5abc2436e7cd3eee50abdac70c518c29ce
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-12-07 11:03:27 +0700

    first init

---
+++ view/barang.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h1>Barang <small>Data Barang</small></h1>
+        <ol class="breadcrumb">
+            <li><a href="index.html"><i class="icon-dashboard"></i> Barang</a></li>
+        </ol>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Barang</th>
+                    <th>Harga Barang</th>
+                    <th>Stok Barang</th>
+                    <th>Gambar Barang</th>
+                    <th>Opsi</th>
+                </tr>
+                <tr>
+                    <td align="center">1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td align="center">
+                        <button class="btn btn-info btn-xs"><i class="fa fa-edit"></i> Edit</button>
+                        <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Hapus</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+</div>
